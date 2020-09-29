2e6c811b6513d0bb39487c72b436a5394c97c97e
JarlTuxen <jarl.tuxen@gmail.com>
2020-09-29 17:12:05 +0200

    first commit

---
+++ src/main/java/dk/kea/dat19c/spring_mvc_mock/SpringMvcMockApplication.java
@@ -0,0 +1,13 @@
+package dk.kea.dat19c.spring_mvc_mock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcMockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcMockApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dk/kea/dat19c/spring_mvc_mock/SpringMvcMockApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.kea.dat19c.spring_mvc_mock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcMockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
